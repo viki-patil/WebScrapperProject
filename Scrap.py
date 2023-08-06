e0ad79081daab136678b24d59dd6ea4ee7c9e319
viki-patil <vikipatil1533@gmail.com>
2023-08-06 10:34:39 +0530

    Complete program

--- Scrap.py
+++ Scrap.py
@@ -33,34 +33,47 @@ def index():
             prodRes = requests.get(productLink)
             prodRes.encoding='utf-8'
             prod_html = bs(prodRes.text, "html.parser")
-            commentboxes = prod_html.find_all('div', {'class': "_16PBlm"})
-            page_num =findNumberOfPages(prod_html)
-            child= prod_html.find('div', {'class': "_3UAT2v _16PBlm"})
-            a_tag = child.find_parent('a')
-            href_value = a_tag.get('href')
-            pages_link = "https://www.flipkart.com" +href_value +"&page="
-            print(href_value)
-            rev_text=[]
-            # page_num=input("Enter the pages")
-            for i in range(1,int(page_num)+1):
-            # for i in range(1,11):
-                flipcart_url_i = pages_link+f"{i}"
-                print(flipcart_url_i)
-                req=requests.get(flipcart_url_i)
-                content=bs(req.content,'html.parser')
-                review=content.find_all('p',{"class":'_2sc7ZR _2V5EHH'})
-                for rt in review:
-                    rev_text.append(rt.text)
-
-            for i in rev_text:
-                print(i)
-            reviews = [{"Product": "searchString", "Name": "name", "Rating": "rating", "CommentHead": "commentHead",
-                          "Comment": "custComment"}]
             
+            # find total number of pages we need to scroll
+            page_num = findNumberOfPages(prod_html)
 
+            # find the flipkarts page links to scroll
+            pages_link = getPageLinks(prod_html)
+            
+            rev_text=[]
+            for i in range(1,int(page_num)+1):
+                # Navigate to each pagelink to capture data
+                flipcart_url_i = pages_link+f"{i}"
+                # print(flipcart_url_i)
+                req=requests.get(flipcart_url_i)
+                
+                # Capture each review individually
+                # review=content.find_all('p',{"class":'_2sc7ZR _2V5EHH'})
+                content=bs(req.content,'html.parser')
+                reviewBoxes = content.select('div._1AtVbE.col-12-12 div._27M-vq')
+                for reviewBox in reviewBoxes:
+                    # print('reviewBox')
+                    try:
+                        name = reviewBox.select('div.row p._2sc7ZR._2V5EHH')[0].text
+                        rating = reviewBox.select('div._3LWZlK._1BLPMq')[0].text
+                        commentHead = reviewBox.select('p._2-N8zT')[0].text
+                        custComment = reviewBox.select('div.t-ZTKy div div')[0].text
+                    except:
+                        logging.info(e)
+                        print(e)
+                        return 'Something wrong while fetching review details'
+                    reviewDict = {
+                        "Product": searchString, 
+                        "Name": name, 
+                        "Rating": rating, 
+                        "CommentHead": commentHead,
+                        "Comment": custComment
+                        }
+                    #print('reviewDict')
+                    #print(reviewDict)
+                    rev_text.append(reviewDict)
 
-            return render_template('results.html', reviews=reviews)
-            # return render_template('result.html', reviews=reviews[0:(len(reviews)-1)])
+            return render_template('results.html', reviews=rev_text)
         except Exception as e:
             logging.info(e)
             print(e)
@@ -74,15 +87,25 @@ def findNumberOfPages(html):
     print(total_review_text)
     total_review=int(total_review_text.split(" ")[1])
     print(total_review)
-    reveiw_per_page = 10
-    total_pages= math.ceil(total_review/reveiw_per_page)
+    review_per_page = 10
+    total_pages= math.ceil(total_review/review_per_page)
     print(total_pages)
     return total_pages
 
+def getPageLinks(html):
+    uniqueChild= html.find('div', {'class': "_3UAT2v _16PBlm"})
+    a_tag = uniqueChild.find_parent('a')
+    href_value = a_tag.get('href')
+    return "https://www.flipkart.com" +href_value +"&page="
 
-if (__name__=="__main__"):
+if __name__=="__main__":
     # app.run(debug=True)
     app.run(host="0.0.0.0")
+
+
+
+
+    
 # flipcart_url = "https://www.flipkart.com/search?q=" + "iphone12pro"
 # urlclient = urlopen(flipcart_url)
 # flipcart_page = urlclient.read()
